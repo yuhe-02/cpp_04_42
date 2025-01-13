70b7508affc59040085a696df5d5e97fdfa2ac38
Yuhei Yamasaki <yyamasak@c6r3s10.42tokyo.jp>
2025-01-13 20:27:45 +0900

    finish ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,24 +1,20 @@
-#include "includes/ClapTrap.hpp"
 #include <iostream>
+#include "../includes/Animal.hpp"
+#include "../includes/Cat.hpp"
+#include "../includes/Dog.hpp"
 
-int main() {
-	ClapTrap ct("test");
-	ClapTrap df(ct);
-	unsigned int test = 4294967295;
-	std::cout << test << std::endl;
-	df.beRepaired(4294967295);
-	// ct.attack("target");
-	// for (int i = 0; i < 5; i++) {
-	// 	df.beRepaired(1);
-	// }
-	// for (int i = 0; i < 15; i++) {
-	// 	df.attack();
-	// }
-	// for (int i = 0; i < 15; i++) {
-	// 	df.takeDamage(1);
-	// }
-	// for (int i = 0; i < 5; i++) {
-	// 	df.beRepaired(1);
-	// }
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	delete meta;
+	delete j;
+	delete i;
 	return (0);
 }
